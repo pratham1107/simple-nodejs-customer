49286b33b495febd263647e6c18ceea1a69b26e9
Mhatre <prathamesh.mhatre@capgemini.com>
2019-09-12 14:39:44 +0530

    beatify js code

--- app.js
+++ app.js
@@ -6,40 +6,42 @@ app.enable('trust proxy');
 const {Datastore} = require('@google-cloud/datastore');
 const datastore = new Datastore();
 
-const getRecords = (id) => {
-	id = id || ""
-  const query = datastore
-    .createQuery('customers_info')
-    .order('id', {descending: false});
-	if (id != ""){
-	query.filter('id', '=', id)
-	}
-  return datastore.runQuery(query);
+const getCustomers = (id) => {
+    id = id || ""
+    const query = datastore
+        .createQuery('customers_info')
+        .order('id', {
+            descending: false
+        });
+    if (id != "") {
+        query.filter('id', '=', id)
+    }
+    return datastore.runQuery(query);
 };
 
 app.get('/getCustomers', async (req, res, next) => {
-  try {
-	const id =req.query.id
-    const [entities] = await getRecords(id);
-    res.json(entities)
-  } catch (error) {
-    next(error);
-  }
+    try {
+        const id = req.query.id
+        const [entities] = await getCustomers(id);
+        res.json(entities)
+    } catch (error) {
+        next(error);
+    }
 });
 
 app.get('/getCustomer', async (req, res, next) => {
-  try {
-	const id =req.query.id
-    const entities = await getRecords(id);
-	res.json(entities[0][0])
-  } catch (error) {
-    next(error);
-  }
+    try {
+        const id = req.query.id
+        const entities = await getCustomers(id);
+        res.json(entities[0][0])
+    } catch (error) {
+        next(error);
+    }
 });
-	
+
 const PORT = process.env.PORT || 8080;
 app.listen(process.env.PORT || 8080, () => {
-  console.log(`running on port ${PORT}`);
+    console.log(`running on port ${PORT}`);
 });
 
 module.exports = app;
